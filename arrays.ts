0f8558edf8d2dd2506ce7251d10a3091df62976a
PetarAsenov63 <passnv6399@gmail.com>
2025-10-14 22:11:15 +0300

    Added arrays examples

---
+++ arrays.ts
@@ -0,0 +1,13 @@
+// arrays declaration
+let numsArray: number[] = [1,2,3,4,];
+let stringsArray: string[] = ['Pesho', 'Gosho', 'Tosho', 'Ivan'];
+let combinedArray: (string | number)[] = [1, "Pesho", "Ivan", 2];
+
+// access element by index
+stringsArray[0];
+stringsArray[1];
+combinedArray[2];
+
+console.log(stringsArray[0]);
+console.log(stringsArray[1]);
+console.log(numsArray.length);
