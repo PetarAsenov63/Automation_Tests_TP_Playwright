a5fa17332977fc58483e28d57a921775c4c040a2
PetarAsenov63 <passnv6399@gmail.com>
2025-10-16 19:56:03 +0300

    Array methods

--- arrays.ts
+++ arrays.ts
@@ -11,3 +11,14 @@ combinedArray[2];
 console.log(stringsArray[0]);
 console.log(stringsArray[1]);
 console.log(numsArray.length);
+
+// method filter
+let positiveNum: number[] = [2,3,4,5,6];
+let filteredArray: number[] = positiveNum.filter((num: number) => num > 3);
+
+// method sort
+let sortNums: number[] = [100, 2, 30, 5, 40, 6]
+sortNums.sort();
+
+// method push 
+sortNums.push(200);
