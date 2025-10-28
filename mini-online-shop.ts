53341da60595797afe249669e880186a784e7794
PetarAsenov63 <passnv6399@gmail.com>
2025-10-28 17:50:44 +0200

    In class Typescript exercise and Homework task

--- mini-online-shop.ts
+++ mini-online-shop.ts
@@ -18,10 +18,46 @@ const tshirt: Product = {name: "T-Shirt", price: 20, category: "clothing"};
 
 const products: Product[] = [apple, laptop];
 
-const customer: Customer = {
+const customerOne: Customer = {
     name: "John Doe",
     email: "johndoe@mail.com",
     cart: products,
+};
+const customerTwo: Customer = {
+    name: "Jane Smith",
+    email: "janesmith@mail.com",
+    cart: products,
+};
+
+function addProductToCart(customer: Customer, product: Product): void {
+    customer.cart.push(product);
+};
+
+function displayCart(customer: Customer): void {
+    console.log(`Customer: ${customer.name}`);
+    console.log("Cart Contents:");
+    customer.cart.forEach((product, index) => {
+        console.log(`${index + 1}. ${product.name} - ${product.price} (${product.category})`);
+    })
+};
+
+function filterByCategory(products: Product[], category: Category): Product[] {
+    const filteredProducts = products.filter(product => product.category === category);
+     console.log(filteredProducts);
+    return filteredProducts
+};
+
+function calculateTotal(customer: Customer): number {
+    return customer.cart.reduce((total, product) => total + product.price, 0);
+};
+ 
+
+addProductToCart(customerOne, tshirt);
+    displayCart(customerOne);
+    filterByCategory(products, "electronics");
+    const total = calculateTotal(customerOne);
+    console.log(`Total Price: $${total}`);
+
 
     // add second customer
     // function to add product to cart
@@ -30,4 +66,3 @@ const customer: Customer = {
     // function to calculateTotal - calculate total price of products in cart
     // customer.cart[0].price
     // customer.cart[1].price
-};
